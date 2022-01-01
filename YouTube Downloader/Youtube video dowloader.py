cfa22ea496669a6303616bb66f84a246b048923c
PrintedJungle38 <96503062+PrintedJungle38@users.noreply.github.com>
2022-01-01 18:55:51 +0100

    Delete YouTube Downloader directory

--- YouTube Downloader/Youtube video dowloader.py
+++
@@ -1,7 +0,0 @@
-from pytube import YouTube
-link = input("enter the link of youtube video: ")
-yt = YouTube(link)
-ys = yt.streams.get_highest_resolution()
-print("Downloading...")
-ys.download("Video scaricati")
-print("download completato!")
